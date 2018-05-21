6b2363f79e61a83fab79f628bccde0215802ce65
Manoj Gautam <manoj.gautam@olenepal.org>
2018-05-21 17:11:54 +0545

    api updated

--- src/pustakalaya_apps/audio/api/v1/serializers.py
+++ src/pustakalaya_apps/audio/api/v1/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from pustakalaya_apps.audio.models import Audio
 
+
+
 class AudioSerializers(serializers.ModelSerializer):
     class Meta:
         model = Audio
--- src/pustakalaya_apps/document/api/v1/serializers.py
+++ src/pustakalaya_apps/document/api/v1/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
-from pustakalaya_apps.document.models import Document 
+from pustakalaya_apps.document.models import (
+    Document, DocumentFileUpload
+) 
+
+class DocumentFileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = DocumentFileUpload
+        fields = ('file_name', 'document', 'upload',)
 
 class DocumentSerializer(serializers.ModelSerializer):
     """
     Class to serialize document data types. 
     """
 
-    
+    documentfileupload_set = DocumentFileSerializer(many=True)
     class Meta:
         model = Document
         fields = '__all__'
-       
+    
