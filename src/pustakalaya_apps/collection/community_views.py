8983d0a14275f265d001601d32f867c9fc0c5fac
Manoj Gautam <manoj.gautam@olenepal.org>
2018-05-29 11:11:42 +0545

    categories page implemented

--- src/pustakalaya_apps/collection/community_views.py
+++ src/pustakalaya_apps/collection/community_views.py
@@ -6,48 +6,59 @@ from elasticsearch_dsl.connections import connections
 
 def community_detail(request, community_name):
     """Community detail page"""
-
     client = connections.get_connection()
 
-    # s = Search(using=client, index=settings.ES_INDEX).query("match", )
+    context = {} # Context data
 
-    # Query the total number of items in elastic search having the name of this community
-    # es_count = Search(index="pustakalaya").using(client).query("match", communities=community_name).count()
+    def community_detail(community_name):
+        """
+        Function to return the list of collection name and total item from this collection.
+        """
+        # Get all the collections from community.
+        collection_list = []
+        all_total = 0
+        collections = Collection.objects.filter(community_name=community_name)
+        
+        for collection in collections:
+            item_count_per_collection = Search(index="pustakalaya").using(client).query("match",collections_ids=collection.pk).count()
+            all_total += item_count_per_collection
+            pk = collection.pk
 
-    # Context data
-    context = {}
-    # print("community name =",community_name)
-
-    community_name = " ".join(community_name.split("-"))
-
-    # Query all the collection that contains this community_name from ORM
-    collections = Collection.objects.filter(community_name=community_name)
-
-    collection_list = []
-
-    all_total = 0
-
-    for collection in collections:
-
-        item_count_per_collection = Search(index="pustakalaya").using(client).query("match",collections_ids=collection.pk).count()
-
-
-        all_total += item_count_per_collection
-
-        pk = collection.pk
-
-        # Create a list to that contain collection_name and total count
-        collection_list.append({
+            # Create a list to that contain collection_name and total count
+            collection_list.append({
             "collection_name": collection.collection_name,
             "total_count": all_total,
             "es_count": item_count_per_collection,
             "pk": pk,
-        })
+            })  
 
-    # Implement total count
+        return {
+            "community_name": community_name,
+            "collection_list": collection_list
+        }
+  
+    # Query literature and arts detail 
+    literature_and_arts = list(map(community_detail, ['literatures and arts']))[0]
+    context["literature_and_arts"] = literature_and_arts
 
-    # Sort list to display in alphabetical order.
-    context["collection_list"] = collection_list
-    context["community_name"] = community_name
-    # print("colllist=",collection_list)
+    # Query course materials 
+    course_materials = list(map(community_detail, ['course materials']))[0]
+    context["course_materials"] = course_materials
+
+    # Query teaching materials
+    teaching_materials = list(map(community_detail, ['teaching materials']))[0]
+    context["teaching_materials"] = teaching_materials
+
+    # Query reference materials detail 
+    reference_materials = list(map(community_detail, ['reference materials']))[0]
+    context["reference_materials"] = reference_materials
+
+    # Query newpapers and magazines detail 
+    newpapers_and_magazines = list(map(community_detail, ['newpapers and magazines']))[0]
+    context["newpapers_and_magazines"] = newpapers_and_magazines
+    
+    # Query newpapers and magazines detail 
+    othereducational_materials = list(map(community_detail, ['othereducational materials']))[0]
+    context["othereducational_materials"] = othereducational_materials
+
     return render(request, "collection/community_detail.html", context)
